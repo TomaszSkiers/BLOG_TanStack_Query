adaa3c9d2adb46223297f16cbe3079ff5bb1d8e0
Tomasz Skierś <tomasz.skiers@gmail.com>
2025-03-15 08:13:35 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Typography } from '@mui/material'
+
+function App() {
+  return (
+    <Typography variant="h4" color="white">
+      {' '}
+      Witaj z Bloga
+    </Typography>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material"
+
+
+
+
+export const Home: React.FC = () => {
+
+
+    return (
+        <Typography variant="h4" color="white"> welcome from Home component</Typography>
+    )
+}
